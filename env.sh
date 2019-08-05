64f5a609d7a3269f9bfdbb198a67dace1452adeb
tamimcse <tamim@csebuet.org>
2019-08-04 20:13:49 -0400

    .

--- env.sh
+++ env.sh
@@ -1,7 +1,8 @@
-export LLVM_HOME=/home/tamim/clang+llvm-3.4-x86_64-linux-gnu-ubuntu-13.10
+export LLVM_HOME=/home/tamim/LLVM-Tracer/build/autoinstaller/build-llvm
 export PATH=$LLVM_HOME/bin/:$PATH
 export LD_LIBRARY_PATH=$LLVM_HOME/lib/:$LD_LIBRARY_PATH
 export ALADDIN_HOME=/home/tamim/ALADDIN
 export BOOST_ROOT=/home/tamim/boost_1_55_0
 export LD_LIBRARY_PATH=$BOOST_ROOT/stage/lib:$LD_LIBRARY_PATH
+export TRACER_HOME=/usr/local
 export TRACER_HOME=/home/tamim/LLVM-Tracer
