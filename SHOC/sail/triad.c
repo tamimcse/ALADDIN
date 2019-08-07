c01d1e79e2ada5626c9e0e280967f78d56deb63d
tamimcse <tamim@csebuet.org>
2019-08-06 20:51:26 -0400

    .

--- SHOC/sail/triad.c
+++ SHOC/sail/triad.c
@@ -1,6 +1,7 @@
 #include "triad.h"
 
-void triad(uint8_t *N16, uint16_t *C16,uint8_t *N24, uint16_t *C24, uint64_t ip){
+void triad(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N32, uint16_t *C32, uint8_t *N40, uint16_t *C40, uint8_t *N48, uint16_t *C48,
+		uint8_t *N56, uint16_t *C56, uint8_t *N64, uint64_t ip){
   int i;
     N16[0] = N24[0] + ip*C24[0];
 }
@@ -13,6 +14,16 @@ int main(){
     C16 = (uint16_t *) malloc (sizeof(uint16_t) * LEVEL16_SIZE);
     N24 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
     C24 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
+    N32 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
+    C32 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
+    N40 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
+    C40 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
+    N48 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
+    C48 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
+    N56 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
+    C56 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
+    N64 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
+
 
 	int i;
   srand(time(NULL));
@@ -21,8 +32,17 @@ int main(){
 		C16[i] = 1;
 		N24[i] = rand();
 		C24[i] = rand();
+		N32[i] = rand();
+		C32[i] = rand();
+		N40[i] = rand();
+		C40[i] = 1;
+		N48[i] = 1;
+		C48[i] = 1;
+		N56[i] = 1;
+		C56[i] = 1;
+		N64[i] = 1;
 	}
-	triad(&N16[0], &C16[0], &N24[0], &C24[0], 67);
+	triad(&N16[0], &C16[0], &N24[0], &C24[0], &N32[0], &C32[0], &N40[0], &C40[0], &N48[0], &C48[0], &N56[0], &C56[0], &N64[0], 67);
 
   FILE *output;
   output = fopen("output.data", "w");
--- SHOC/sail/triad.h
+++ SHOC/sail/triad.h
@@ -7,4 +7,5 @@
 #define NUM_CHUNKS 16384
 #define ARR_SIZE NUM_CHUNKS * 256
 
-void triad(uint8_t *N16, uint16_t *C16,uint8_t *N24, uint16_t *C24, uint64_t ip);
+void triad(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N32, uint16_t *C32, uint8_t *N40, uint16_t *C40, uint8_t *N48, uint16_t *C48,
+		uint8_t *N56, uint16_t *C56, uint8_t *N64, uint64_t ip);
