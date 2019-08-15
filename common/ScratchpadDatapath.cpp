70cdfa62e54f522d0086d66ebcdc4880b1cfafaf
tamimcse <tamim@csebuet.org>
2019-08-14 20:45:05 -0400

    .

--- common/ScratchpadDatapath.cpp
+++ common/ScratchpadDatapath.cpp
@@ -288,6 +288,13 @@ void ScratchpadDatapath::stepExecutingQueue() {
   }
 }
 
+/*As Late As Possible (ALAP) rescheduling for non-memory, non-control nodes.
+  The first pass of scheduling is as early as possible, whenever a node's
+  parents are ready, the node is executed. This mode of executing potentially
+  can lead to values that are produced way earlier than they are needed. For
+  such case, we add an ALAP rescheduling pass to reorganize the graph without
+  changing the critical path and memory nodes, but produce a more balanced
+  design.*/
 int ScratchpadDatapath::rescheduleNodesWhenNeeded() {
   std::vector<Vertex> topo_nodes;
   boost::topological_sort(program.graph, std::back_inserter(topo_nodes));
