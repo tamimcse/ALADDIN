fcc329850a4d5e96820941b03a001e8769c3c6ff
tamimcse <tamim@csebuet.org>
2019-09-07 18:56:18 -0400

    .

--- SHOC/branch/br.c
+++ SHOC/branch/br.c
@@ -1,7 +1,6 @@
 #include "br.h"
 
-uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N32, uint16_t *C32, uint8_t *N40, uint16_t *C40, uint8_t *N48, uint16_t *C48,
-		uint8_t *N56, uint16_t *C56, uint8_t *N64, uint64_t ip){
+uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N32, uint64_t ip){
     uint16_t nix;
     uint32_t cix;
     uint8_t nh;
@@ -27,23 +26,14 @@ uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N3
 
 int main(){
 	uint8_t nh;
-	uint8_t *N16, *N24, *N32, *N40, *N48, *N56, *N64;
-	uint16_t *C16, *C24, *C32, *C40, *C48, *C56;
+	uint8_t *N16, *N24, *N32;
+	uint16_t *C16, *C24;
 
     N16 = (uint8_t *) malloc (sizeof(uint8_t) * LEVEL16_SIZE);
     C16 = (uint16_t *) malloc (sizeof(uint16_t) * LEVEL16_SIZE);
     N24 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
     C24 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
     N32 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
-    C32 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
-    N40 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
-    C40 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
-    N48 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
-    C48 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
-    N56 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
-    C56 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
-    N64 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
-
 
 	int i;
   srand(time(NULL));
@@ -53,16 +43,8 @@ int main(){
 		N24[i] = rand();
 		C24[i] = rand();
 		N32[i] = rand();
-		C32[i] = rand();
-		N40[i] = rand();
-		C40[i] = 1;
-		N48[i] = 1;
-		C48[i] = 1;
-		N56[i] = 1;
-		C56[i] = 1;
-		N64[i] = 1;
 	}
-	nh = br(&N16[0], &C16[0], &N24[0], &C24[0], &N32[0], &C32[0], &N40[0], &C40[0], &N48[0], &C48[0], &N56[0], &C56[0], &N64[0], 67);
+	nh = br(&N16[0], &C16[0], &N24[0], &C24[0], &N32[0], 67);
 
   FILE *output;
   output = fopen("output.data", "w");
