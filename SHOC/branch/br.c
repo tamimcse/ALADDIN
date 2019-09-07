858bea10b385408752596b867d0c6edaab0163ca
tamimcse <tamim@csebuet.org>
2019-09-07 19:02:37 -0400

    .

--- SHOC/branch/br.c
+++ SHOC/branch/br.c
@@ -1,6 +1,6 @@
 #include "br.h"
 
-uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N32, uint64_t ip){
+uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint64_t ip){
     uint16_t nix;
     uint32_t cix;
     uint8_t nh;
@@ -14,11 +14,6 @@ uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N3
 	nix = (C16[nix] - 1) * 256 + ((ip >> 40) & 0XFF);
         if (N24[nix])
 	    nh = N24[nix];
-        if (C24[nix]) {
-	    nix = (C24[nix] - 1) * 256 + ((ip >> 32) & 0XFF);
-            if (N32[nix])
-	        nh = N32[nix];
-        }
     }
 
     return nh;
@@ -26,14 +21,12 @@ uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N3
 
 int main(){
 	uint8_t nh;
-	uint8_t *N16, *N24, *N32;
-	uint16_t *C16, *C24;
+	uint8_t *N16, *N24;
+	uint16_t *C16;
 
     N16 = (uint8_t *) malloc (sizeof(uint8_t) * LEVEL16_SIZE);
     C16 = (uint16_t *) malloc (sizeof(uint16_t) * LEVEL16_SIZE);
     N24 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
-    C24 = (uint16_t *) malloc (sizeof(uint16_t) * ARR_SIZE);
-    N32 = (uint8_t *) malloc (sizeof(uint8_t) * ARR_SIZE);
 
 	int i;
   srand(time(NULL));
@@ -41,10 +34,8 @@ int main(){
 		N16[i] = 1;
 		C16[i] = 1;
 		N24[i] = rand();
-		C24[i] = rand();
-		N32[i] = rand();
 	}
-	nh = br(&N16[0], &C16[0], &N24[0], &C24[0], &N32[0], 67);
+	nh = br(&N16[0], &C16[0], &N24[0], 67);
 
   FILE *output;
   output = fopen("output.data", "w");
--- SHOC/branch/br.h
+++ SHOC/branch/br.h
@@ -7,4 +7,4 @@
 #define NUM_CHUNKS 16384
 #define ARR_SIZE NUM_CHUNKS * 256
 
-uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint16_t *C24, uint8_t *N32, uint64_t ip);
+uint8_t br(uint8_t *N16, uint16_t *C16, uint8_t *N24, uint64_t ip);
