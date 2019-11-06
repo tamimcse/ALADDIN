dc4f1b3c6eed24a55ac0ecccd7ed80e1dbedcdab
tamimcse <tamim@csebuet.org>
2019-11-06 01:02:14 -0500

    Removed pdf creation in poptrie

--- SHOC/poptrie/run-alladin.sh
+++ SHOC/poptrie/run-alladin.sh
@@ -1,3 +1,3 @@
 make run-trace &&
 $ALADDIN_HOME/common/aladdin poptrie dynamic_trace.gz example/config_example
-dot -Tpdf poptrie_graph.dot -o poptrie_graph.pdf
+#dot -Tpdf poptrie_graph.dot -o poptrie_graph.pdf
