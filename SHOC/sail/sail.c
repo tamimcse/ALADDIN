11809417774d54252064b4ef4c7d81f4d2ff8a0a
tamimcse <tamim@csebuet.org>
2019-11-06 00:54:41 -0500

    new SAIL properly implemented in Aladdin

--- SHOC/sail/sail.c
+++ SHOC/sail/sail.c
@@ -42,7 +42,7 @@
 static int sail_init (struct sail *sail) {
   int err = 0;
 
-  sail->def_nh = 10;
+  sail->def_nh = 1;
   err = sail_level_init (&sail->level16, 16, SIZE16, CNK_8, NULL);
   err = sail_level_init (&sail->level24, 24, SIZE24, CNK_8, &sail->level16);
   err = sail_level_init (&sail->level32, 32, SIZE32, CNK_8, &sail->level24);
@@ -61,23 +61,23 @@ static int sail_init (struct sail *sail) {
 
   return err;
 }
-/*
-uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, uint8_t *N32, uint32_t *C32, uint8_t *N40, uint32_t *C40,
+
+uint8_t fib_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, uint8_t *N32, uint32_t *C32, uint8_t *N40, uint32_t *C40,
                     uint8_t *N48, uint32_t *C48, uint8_t *N56, uint32_t *C56, uint8_t *N64, uint32_t *C64, uint8_t *N72, uint32_t *C72,
                     uint8_t *N80, uint32_t *C80, uint8_t *N88, uint32_t *C88, uint8_t *N96, uint32_t *C96, uint8_t *N104, uint32_t *C104,
                     uint8_t *N112, uint32_t *C112, uint8_t *N120, uint32_t *C120, uint8_t *N128, uint8_t def_nh,
-                    __uint128_t key) {
+                    uint64_t key1, uint64_t key2) {
 
   uint32_t idx;
   uint8_t nh = def_nh;
 
-  idx = key >> 112;
+  idx = key1 >> 48;
 
   if (N16[idx] != 0)
     nh = N16[idx];
 
   if (C16[idx] != 0) {
-    idx = (C16[idx] - 1) * CNK_8 + ((key >> 104) & 0XFF);
+    idx = (C16[idx] - 1) * CNK_8 + ((key1 >> 40) & 0XFF);
   } else {
     goto finish;
   }
@@ -86,7 +86,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N24[idx];
     
   if (C24[idx] != 0) {
-    idx = (C24[idx] - 1) * CNK_8 + ((key >> 96) & 0XFF);
+    idx = (C24[idx] - 1) * CNK_8 + ((key1 >> 32) & 0XFF);
   } else {
     goto finish;
   }        
@@ -95,7 +95,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N32[idx];
 
   if (C32[idx] != 0) {
-    idx = (C32[idx] - 1) * CNK_8 + ((key >> 88) & 0XFF);
+    idx = (C32[idx] - 1) * CNK_8 + ((key1 >> 24) & 0XFF);
   } else {
     goto finish;
   }
@@ -104,7 +104,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N40[idx];
 
   if (C40[idx] != 0) {
-    idx = (C40[idx] - 1) * CNK_8 + ((key >> 80) & 0XFF);
+    idx = (C40[idx] - 1) * CNK_8 + ((key1 >> 16) & 0XFF);
   } else {
     goto finish;
   }
@@ -113,7 +113,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N48[idx];
 
   if (C48[idx] != 0) {
-    idx = (C48[idx] - 1) * CNK_8 + ((key >> 72) & 0XFF);
+    idx = (C48[idx] - 1) * CNK_8 + ((key1 >> 8) & 0XFF);
   } else {
     goto finish;
   }
@@ -122,7 +122,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N56[idx];
 
   if (C56[idx] != 0) {
-    idx = (C56[idx] - 1) * CNK_8 + ((key >> 64) & 0XFF);
+    idx = (C56[idx] - 1) * CNK_8 + (key1 & 0XFF);
   } else {
     goto finish;
   }
@@ -131,7 +131,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N64[idx];
 
   if (C64[idx] != 0)
-    idx = (C64[idx] - 1) * CNK_8 + ((key >> 56) & 0XFF);
+    idx = (C64[idx] - 1) * CNK_8 + ((key2 >> 56) & 0XFF);
   else
     goto finish;
 
@@ -139,7 +139,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N72[idx];
 
   if (C72[idx] != 0)
-    idx = (C72[idx] - 1) * CNK_8 + ((key >> 48) & 0XFF);
+    idx = (C72[idx] - 1) * CNK_8 + ((key2 >> 48) & 0XFF);
   else
     goto finish;
 
@@ -147,7 +147,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N80[idx];
 
   if (C80[idx] != 0)
-    idx = (C80[idx] - 1) * CNK_8 + ((key >> 40) & 0XFF);
+    idx = (C80[idx] - 1) * CNK_8 + ((key2 >> 40) & 0XFF);
   else
     goto finish;
 
@@ -155,7 +155,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N88[idx];
 
   if (C88[idx] != 0)
-    idx = (C88[idx] - 1) * CNK_8 + ((key >> 32) & 0XFF);
+    idx = (C88[idx] - 1) * CNK_8 + ((key2 >> 32) & 0XFF);
   else
     goto finish;
 
@@ -163,7 +163,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N96[idx];
 
   if (C96[idx] != 0)
-    idx = (C96[idx] - 1) * CNK_8 + ((key >> 24) & 0XFF);
+    idx = (C96[idx] - 1) * CNK_8 + ((key2 >> 24) & 0XFF);
   else
     goto finish;
 
@@ -171,7 +171,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N104[idx];
 
   if (C104[idx] != 0)
-    idx = (C104[idx] - 1) * CNK_8 + ((key >> 16) & 0XFF);
+    idx = (C104[idx] - 1) * CNK_8 + ((key2 >> 16) & 0XFF);
   else
     goto finish;
 
@@ -179,7 +179,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N112[idx];
 
   if (C112[idx] != 0)
-    idx = (C112[idx] - 1) * CNK_8 + ((key >> 8) & 0XFF);
+    idx = (C112[idx] - 1) * CNK_8 + ((key2 >> 8) & 0XFF);
   else
     goto finish;
 
@@ -187,7 +187,7 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
     nh = N120[idx];
 
   if (C120[idx] != 0)
-    idx = (C120[idx] - 1) * CNK_8 + (key & 0XFF);
+    idx = (C120[idx] - 1) * CNK_8 + (key2 & 0XFF);
   else
     goto finish;
 
@@ -197,52 +197,6 @@ uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, ui
 finish:
   return nh;
 }
-*/
-
-uint8_t fib_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, uint8_t *N32, uint32_t *C32, uint8_t *N40, uint32_t *C40, uint8_t *N48, uint32_t *C48,
-		uint8_t *N56, uint32_t *C56, uint8_t *N64, uint64_t ip){
-    uint16_t nix;
-    uint32_t cix;
-    uint8_t nh;
-
-    nix = ip >> 48;
-
-    if (N16[nix])
-	nh = N16[nix];
-
-    if (C16[nix]) {
-	nix = (C16[nix] - 1) * 256 + ((ip >> 40) & 0XFF);
-        if (N24[nix])
-	    nh = N24[nix];
-        if (C24[nix]) {
-	    nix = (C24[nix] - 1) * 256 + ((ip >> 32) & 0XFF);
-            if (N32[nix])
-	        nh = N32[nix];
-	    if (C32[nix]) {
-		nix = (C32[nix] - 1) * 256 + ((ip >> 24) & 0XFF);
-	    	if (N40[nix])
-		    nh = N40[nix];
-	        if (C40[nix]) {
-		    nix = (C40[nix] - 1) * 256 + ((ip >> 16) & 0XFF);
-		    if (N48[nix])
-			nh = N48[nix];
-		    if (C48[nix]) {
-		        nix = (C48[nix] - 1) * 256 + ((ip >> 8) & 0XFF);
-		        if (N56[nix])
-			    nh = N56[nix];
-		        if (C56[nix]) {
-			    nix = (C56[nix] - 1) * 256 + (ip & 0XFF);
-		            if (N64[nix])
-			        nh = N64[nix];
-		        }
-    		    }
-	        }
-    	    }
-        }
-    }
-
-    return nh;
-}
 
 int main(){
   struct sail sail;
@@ -250,8 +204,10 @@ int main(){
   
   sail_init(&sail);
 
-  nh = fib_lookup(sail.level16.N, sail.level16.N, sail.level24.N, sail.level24.C, sail.level32.N, sail.level32.C, sail.level40.N, sail.level40.C, sail.level48.N,
-                  sail.level48.C, sail.level56.N, sail.level56.C, sail.level64.N, 67);
+  nh = fib_lookup(sail.level16.N, sail.level16.C, sail.level24.N, sail.level24.C, sail.level32.N, sail.level32.C, sail.level40.N, sail.level40.C,
+                  sail.level48.N, sail.level48.C, sail.level56.N, sail.level56.C, sail.level64.N, sail.level64.C, sail.level72.N, sail.level72.C,
+                  sail.level80.N, sail.level80.C, sail.level88.N, sail.level88.C, sail.level96.N, sail.level96.C, sail.level104.N, sail.level104.C,
+                  sail.level112.N, sail.level112.C, sail.level120.N, sail.level120.C, sail.level128.N, sail.def_nh, 67, 65);
 
   FILE *output;
   output = fopen("output.data", "w");
--- SHOC/sail/sail.h
+++ SHOC/sail/sail.h
@@ -45,13 +45,9 @@ struct sail {
   struct sail_level level16, level24, level32, level40, level48, level56, level64, level72, level80, level88, level96, level104, level112, level120, level128; 
 };
 
-/*
-uint8_t sail_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, uint8_t *N32, uint32_t *C32, uint8_t *N40, uint32_t *C40,
+
+uint8_t fib_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, uint8_t *N32, uint32_t *C32, uint8_t *N40, uint32_t *C40,
                     uint8_t *N48, uint32_t *C48, uint8_t *N56, uint32_t *C56, uint8_t *N64, uint32_t *C64, uint8_t *N72, uint32_t *C72,
                     uint8_t *N80, uint32_t *C80, uint8_t *N88, uint32_t *C88, uint8_t *N96, uint32_t *C96, uint8_t *N104, uint32_t *C104,
                     uint8_t *N112, uint32_t *C112, uint8_t *N120, uint32_t *C120, uint8_t *N128, uint8_t def_nh,
-                    __uint128_t key);*/
-
-
-uint8_t fib_lookup(uint8_t *N16, uint32_t *C16, uint8_t *N24, uint32_t *C24, uint8_t *N32, uint32_t *C32, uint8_t *N40, uint32_t *C40, uint8_t *N48, uint32_t *C48,
-		uint8_t *N56, uint32_t *C56, uint8_t *N64, uint64_t ip);
+                    uint64_t ip1, uint64_t ip2);
