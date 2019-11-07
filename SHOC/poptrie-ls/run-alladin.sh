342a34ca6fe26043f94fbaf6397f427efc5d2ae8
tamimcse <tamim@csebuet.org>
2019-11-07 00:58:28 -0500

    .

--- SHOC/poptrie-ls/run-alladin.sh
+++ SHOC/poptrie-ls/run-alladin.sh
@@ -1,3 +1,3 @@
 make run-trace &&
-$ALADDIN_HOME/common/aladdin poptrie dynamic_trace.gz example/config_example
+$ALADDIN_HOME/common/aladdin poptrie-ls dynamic_trace.gz example/config_example
 #dot -Tpdf poptrie_graph.dot -o poptrie_graph.pdf
